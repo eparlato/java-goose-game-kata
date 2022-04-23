e277d9d2377b60d892122252c7ecf7f7c7dfa6f1
Edoardo Parlato <eparlato82@gmail.com>
2022-04-23 18:02:24 +0200

    First commit, with junit5, assertj and mockito

---
+++ src/test/java/it/eparlato/goosegame/GooseGameTest.java
@@ -0,0 +1,12 @@
+package it.eparlato.goosegame;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class GooseGameTest {
+    @Test
+    void uses_junit_correctly() {
+        assertThat(true).isTrue();
+    }
+}
